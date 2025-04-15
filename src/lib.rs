a8659895a4b7d4f61481b523e086cc5dd560f69c
Ifropc <ifropc@apache.org>
2025-04-15 16:57:20 -0700

    Serde stuff

--- src/lib.rs
+++ src/lib.rs
@@ -622,14 +622,23 @@ pub enum EventStart {
 #[derive(serde::Deserialize, serde::Serialize, Debug, Clone)]
 pub struct FullLedgerEntry {
     pub key: LedgerKey,
+    // Should we rename it to XDR to match LedgerEntry output?
     pub val: LedgerEntryData,
+    #[serde(rename = "lastModifiedLedgerSeq")]
     pub last_modified_ledger: u32,
-    pub live_until_ledger_seq: u32,
+    #[serde(
+        rename = "liveUntilLedgerSeq",
+        skip_serializing_if = "Option::is_none",
+        deserialize_with = "deserialize_option_number_from_string",
+        default
+    )]
+    pub live_until_ledger_seq: Option<u32>,
 }
 
 #[derive(serde::Deserialize, serde::Serialize, Debug, Clone)]
 pub struct FullLedgerEntries {
     pub entries: Vec<FullLedgerEntry>,
+    #[serde(rename = "latestLedger")]
     pub latest_ledger: i64,
 }
 
@@ -992,7 +1001,7 @@ impl Client {
                     Ok(FullLedgerEntry {
                         key: LedgerKey::from_xdr_base64(key, Limits::none())?,
                         val: LedgerEntryData::from_xdr_base64(xdr, Limits::none())?,
-                        live_until_ledger_seq: live_until_ledger_seq_ledger_seq.unwrap_or_default(),
+                        live_until_ledger_seq: *live_until_ledger_seq_ledger_seq,
                         last_modified_ledger: *last_modified_ledger,
                     })
                 },
